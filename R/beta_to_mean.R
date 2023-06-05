fa54383bd85d304e2183616143e99ade7db3b4b3
Natasha Klappstein <nklappst@ualberta.ca>
2023-06-05 11:10:30 -0300

    Function exports, Rcpp functions

--- R/beta_to_mean.R
+++ R/beta_to_mean.R
@@ -1,23 +1,25 @@
-##' Get the mean and sd from estimated betas 
-##' 
+##' Get the mean and sd from estimated betas
+##'
 ##' @param fit fitted model output
 ##' @param n_states number of fitted states
+##'
+##' @export
 
 beta_to_mean <- function(fit, n_states) {
-  
+
   # calculate mean from betas
-  mean_step <- (fit$betas$estimate[which(fit$betas$cov == "log(step)")] + 2) / 
+  mean_step <- (fit$betas$estimate[which(fit$betas$cov == "log(step)")] + 2) /
     fit$betas$estimate[which(fit$betas$cov == "step")] * - 1
-  
+
   # calculate sd from betas
   sd_step <- sqrt(fit$betas$estimate[which(fit$betas$cov == "log(step)")] + 2) /
     fit$betas$estimate[which(fit$betas$cov == "step")] * - 1
-  
+
   # create dataframe to return
-  mean_df <- data.frame(state = rep(1:n_states), 
-                        mean = mean_step, 
+  mean_df <- data.frame(state = rep(1:n_states),
+                        mean = mean_step,
                         sd = sd_step)
-  
+
   return(mean_df)
 }
-  
+
---
+++ inst/zebra_test.R
@@ -0,0 +1,22 @@
+library(hmmSSF)
+
+# load data
+data <- readRDS("inst/zebra_processed.RData")
+initial_par <- readRDS("inst/initial_par.RData")
+initial_par <- initial_par[[3]]
+
+# model settings
+n_states <- 2
+ssf_formula <- ~ step + log(step) + cos(angle) + veg
+tpm_formula <- ~ cos(2*pi*tod/24) + sin(2*pi*tod/24)
+
+# fit model
+fit <- fitHMMSSF(ssf_formula = ssf_formula,
+                 tpm_formula = tpm_formula,
+                 data = data,
+                 par0 = initial_par,
+                 n_states = n_states,
+                 dist = "gamma",
+                 optim_opts = list(trace = 1,
+                                   maxit = 1e4))
+fit$betas
