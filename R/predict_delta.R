237552297aa6ed2360601080961273b7c72ce2f9
Théo Michelot <tm75@st-andrews.ac.uk>
2023-06-06 16:28:20 -0300

    fix predict_delta() and predict_tpm()

--- R/predict_delta.R
+++ R/predict_delta.R
@@ -1,73 +1,78 @@
 
-##' Predict stationary state probabilities (with uncertainty)
-##'
-##' @param ...
-##'
-##' @return List with elements 'mle', 'upper', and 'lower'.
-##' Each element is an array, where each layer is a transition
-##' probability matrix corresponding to a row of 'new_data'.
-##'
-##' @export
-predict_delta <- function(tpm_formula,
-                        new_data,
-                        fit,
-                        n_states,
-                        return_CI = TRUE) {
+#' Predict stationary state probabilities (with uncertainty)
+#'
+#' @param mod Fitted model object, as returned by \code{\link{fitHMMSSF}}
+#' @param new_data Data frame with covariate values to use for prediction
+#' @param return_CI Logical (default: TRUE). Should confidence intervals be
+#' computed?
+#'
+#' @return List with elements 'mle', 'upper', and 'lower'.
+#' Each element is an array, where each layer is a transition
+#' probability matrix corresponding to a row of 'new_data'.
+#'
+#' @export
 
-    # separate parameters from list
-    tpm_par <- matrix(fit$tpm_par$estimate, ncol = n_states^2 - n_states)
+predict_delta <- function(mod,
+                          new_data,
+                          return_CI = TRUE) {
 
-    # get Gamma from TP model matrix
-    options(na.action = 'na.pass')
-    tpm_MM <- model.matrix(tpm_formula, new_data)
-    Gamma <-  moveHMM:::trMatrix_rcpp(nbStates = n_states,
-                                      beta = tpm_par,
-                                      covs = tpm_MM)
-    delta <- t(apply(Gamma, 3, function(tpm) {
-        solve(t(diag(n_states) - tpm + 1), rep(1, n_states))
-    }))
-    colnames(delta) <- 1:n_states
-    out <- list(mle = delta)
+  n_states <- mod$args$n_states
+  tpm_formula <- mod$args$tpm_formula
 
-    if(return_CI) {
-        # get covariance matrix
-        par_ind <- (nrow(fit$ssf_par) + 1) : (nrow(fit$ssf_par) + nrow(fit$tpm_par))
-        Sigma <- solve(fit$hessian)[par_ind, par_ind]
+  # separate parameters from list
+  tpm_par <- matrix(mod$par$tpm$estimate, ncol = n_states^2 - n_states)
 
-        # for differentiation to obtain confidence intervals (delta method)
-        get_delta <- function(alpha, modmat, n_states, i) {
-            tpm <- moveHMM:::trMatrix_rcpp(nbStates = n_states,
-                                             beta = alpha,
-                                             covs = modmat)[,,1]
-            delta <- solve(t(diag(n_states) - tpm + 1), rep(1, n_states))
-            return(delta[i])
-        }
+  # get Gamma from TP model matrix
+  options(na.action = 'na.pass')
+  tpm_MM <- model.matrix(tpm_formula, new_data)
+  Gamma <-  moveHMM:::trMatrix_rcpp(nbStates = n_states,
+                                    beta = tpm_par,
+                                    covs = tpm_MM)
+  delta <- t(apply(Gamma, 3, function(tpm) {
+    solve(t(diag(n_states) - tpm + 1), rep(1, n_states))
+  }))
+  colnames(delta) <- 1:n_states
+  out <- list(mle = delta)
 
-        # copy structure of Gamma for upper/lower CI bounds
-        lower <- delta
-        upper <- delta
+  if(return_CI) {
+    # get covariance matrix
+    par_ind <- (nrow(mod$par$ssf) + 1) : (nrow(mod$par$ssf) + nrow(mod$par$tpm))
+    Sigma <- solve(mod$fit$hessian)[par_ind, par_ind]
 
-        # loop over transition probabilities
-        for(i in 1:n_states) {
-                # derive confidence intervals using the delta method
-                dN <- t(apply(tpm_MM, 1, function(x)
-                    numDeriv::grad(get_delta, tpm_par,
-                                   modmat = matrix(x, nrow = 1),
-                                   n_states = n_states, i = i)))
-
-                se <- t(apply(dN, 1, function(x)
-                    suppressWarnings(sqrt(x %*% Sigma %*% x))))
-
-                # transform estimates and standard errors to R, to derive CI
-                # on working scale, then back-transform to [0,1]
-                width <- 1.96 * se / (delta[,i] - delta[,i]^2)
-                lower[,i] <- plogis(qlogis(delta[,i]) - width)
-                upper[,i] <- plogis(qlogis(delta[,i]) + width)
-        }
-
-        out$lower <- lower
-        out$upper <- upper
+    # for differentiation to obtain confidence intervals (delta method)
+    get_delta <- function(alpha, modmat, n_states, i) {
+      tpm <- moveHMM:::trMatrix_rcpp(nbStates = n_states,
+                                     beta = alpha,
+                                     covs = modmat)[,,1]
+      delta <- solve(t(diag(n_states) - tpm + 1), rep(1, n_states))
+      return(delta[i])
     }
 
-    return(out)
+    # copy structure of Gamma for upper/lower CI bounds
+    lower <- delta
+    upper <- delta
+
+    # loop over transition probabilities
+    for(i in 1:n_states) {
+      # derive confidence intervals using the delta method
+      dN <- t(apply(tpm_MM, 1, function(x)
+        numDeriv::grad(get_delta, tpm_par,
+                       modmat = matrix(x, nrow = 1),
+                       n_states = n_states, i = i)))
+
+      se <- t(apply(dN, 1, function(x)
+        suppressWarnings(sqrt(x %*% Sigma %*% x))))
+
+      # transform estimates and standard errors to R, to derive CI
+      # on working scale, then back-transform to [0,1]
+      width <- 1.96 * se / (delta[,i] - delta[,i]^2)
+      lower[,i] <- plogis(qlogis(delta[,i]) - width)
+      upper[,i] <- plogis(qlogis(delta[,i]) + width)
+    }
+
+    out$lower <- lower
+    out$upper <- upper
+  }
+
+  return(out)
 }
--- R/predict_tpm.R
+++ R/predict_tpm.R
@@ -1,72 +1,77 @@
 
-##' Predict transition probabilities (with uncertainty)
-##'
-##' @param ...
-##'
-##' @return List with elements 'mle', 'upper', and 'lower'.
-##' Each element is an array, where each layer is a transition
-##' probability matrix corresponding to a row of 'new_data'.
-##'
-##' @export
-predict_tpm <- function(tpm_formula,
+#' Predict transition probabilities (with uncertainty)
+#'
+#' @param mod Fitted model object, as returned by \code{\link{fitHMMSSF}}
+#' @param new_data Data frame with covariate values to use for prediction
+#' @param return_CI Logical (default: TRUE). Should confidence intervals be
+#' computed?
+#'
+#' @return List with elements 'mle', 'upper', and 'lower'.
+#' Each element is an array, where each layer is a transition
+#' probability matrix corresponding to a row of 'new_data'.
+#'
+#' @export
+
+predict_tpm <- function(mod,
                         new_data,
-                        fit,
-                        n_states,
                         return_CI = TRUE) {
 
-    # separate parameters from list
-    tpm_par <- matrix(fit$tpm_par$estimate, ncol = n_states^2 - n_states)
+  n_states <- mod$args$n_states
+  tpm_formula <- mod$args$tpm_formula
 
-    # get Gamma from TP model matrix
-    options(na.action = 'na.pass')
-    tpm_MM <- model.matrix(tpm_formula, new_data)
-    Gamma <-  moveHMM:::trMatrix_rcpp(nbStates = n_states,
-                                      beta = tpm_par,
-                                      covs = tpm_MM)
-    rownames(Gamma) <- 1:n_states
-    colnames(Gamma) <- 1:n_states
-    out <- list(mle = Gamma)
+  # separate parameters from list
+  tpm_par <- matrix(mod$par$tpm$estimate, ncol = n_states^2 - n_states)
 
-    if(return_CI) {
-        # get covariance matrix
-        par_ind <- (nrow(fit$ssf_par) + 1) : (nrow(fit$ssf_par) + nrow(fit$tpm_par))
-        Sigma <- solve(fit$hessian)[par_ind, par_ind]
+  # get Gamma from TP model matrix
+  options(na.action = 'na.pass')
+  tpm_MM <- model.matrix(tpm_formula, new_data)
+  Gamma <-  moveHMM:::trMatrix_rcpp(nbStates = n_states,
+                                    beta = tpm_par,
+                                    covs = tpm_MM)
+  rownames(Gamma) <- 1:n_states
+  colnames(Gamma) <- 1:n_states
+  out <- list(mle = Gamma)
 
-        # for differentiation to obtain confidence intervals (delta method)
-        get_gamma <- function(alpha, modmat, n_states, i, j) {
-            gamma <- moveHMM:::trMatrix_rcpp(nbStates = n_states,
-                                             beta = alpha,
-                                             covs = modmat)[,,1]
-            return(gamma[i, j])
-        }
+  if(return_CI) {
+    # get covariance matrix
+    par_ind <- (nrow(mod$par$ssf) + 1) : (nrow(mod$par$ssf) + nrow(mod$par$tpm))
+    Sigma <- solve(mod$fit$hessian)[par_ind, par_ind]
 
-        # copy structure of Gamma for upper/lower CI bounds
-        lower <- Gamma
-        upper <- Gamma
-
-        # loop over transition probabilities
-        for(i in 1:n_states) {
-            for(j in 1:n_states) {
-                # derive confidence intervals using the delta method
-                dN <- t(apply(tpm_MM, 1, function(x)
-                    numDeriv::grad(get_gamma, tpm_par,
-                                   modmat = matrix(x, nrow = 1),
-                                   n_states = n_states, i = i, j = j)))
-
-                se <- t(apply(dN, 1, function(x)
-                    suppressWarnings(sqrt(x %*% Sigma %*% x))))
-
-                # transform estimates and standard errors to R, to derive CI
-                # on working scale, then back-transform to [0,1]
-                width <- 1.96 * se / (Gamma[i,j,] - Gamma[i,j,]^2)
-                lower[i,j,] <- plogis(qlogis(Gamma[i,j,]) - width)
-                upper[i,j,] <- plogis(qlogis(Gamma[i,j,]) + width)
-            }
-        }
-
-        out$lower <- lower
-        out$upper <- upper
+    # for differentiation to obtain confidence intervals (delta method)
+    get_gamma <- function(alpha, modmat, n_states, i, j) {
+      gamma <- moveHMM:::trMatrix_rcpp(nbStates = n_states,
+                                       beta = alpha,
+                                       covs = modmat)[,,1]
+      return(gamma[i, j])
     }
 
-    return(out)
+    # copy structure of Gamma for upper/lower CI bounds
+    lower <- Gamma
+    upper <- Gamma
+
+    # loop over transition probabilities
+    for(i in 1:n_states) {
+      for(j in 1:n_states) {
+        # derive confidence intervals using the delta method
+        dN <- t(apply(tpm_MM, 1, function(x)
+          numDeriv::grad(get_gamma, tpm_par,
+                         modmat = matrix(x, nrow = 1),
+                         n_states = n_states, i = i, j = j)))
+
+        se <- t(apply(dN, 1, function(x)
+          suppressWarnings(sqrt(x %*% Sigma %*% x))))
+
+        # transform estimates and standard errors to R, to derive CI
+        # on working scale, then back-transform to [0,1]
+        width <- 1.96 * se / (Gamma[i,j,] - Gamma[i,j,]^2)
+        lower[i,j,] <- plogis(qlogis(Gamma[i,j,]) - width)
+        upper[i,j,] <- plogis(qlogis(Gamma[i,j,]) + width)
+      }
+    }
+
+    out$lower <- lower
+    out$upper <- upper
+  }
+
+  return(out)
 }
