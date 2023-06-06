d3141e9af17ed8f8bf3ad7afa12f139fd6affe1a
Natasha Klappstein <nklappst@ualberta.ca>
2023-06-06 11:08:38 -0300

    fit simulated data

--- inst/sim_data/sim_data_process.R
+++ inst/sim_data/sim_data_process.R
@@ -26,3 +26,34 @@ ggplot(subset(data, stratum %in% 3:4),
            size = factor(obs))) +
   geom_point(alpha = 0.5, size = 0.5) +
   coord_equal()
+
+data$cov1 <- extract(cov_data, as.matrix(data[, c("x", "y")]))
+data$tod <- rep(lubridate::hour(track$time)[-(1:2)], each = 21)
+
+# set model formulation
+ssf_formula <- ~ step + log(step) + cos(angle) + cov1
+tpm_formula <- ~ cos(2 * pi * tod / 24) + sin(2 * pi * tod / 24)
+n_states <- 2
+par0 <- list(betas = matrix(c(-2, -2,
+                            -1, 2,
+                            0.2, 5,
+                            3, -1),
+                            ncol = 2, byrow = TRUE),
+             alphas = matrix(c(-2, -2,
+                               1, -2,
+                               0.1, 1),
+                             ncol = 2, byrow = TRUE))
+
+mod <- fitHMMSSF(ssf_formula = ssf_formula, tpm_formula = tpm_formula,
+                 data = data, par0 = par0, n_states = n_states)
+
+
+
+
+
+
+
+
+
+
+
