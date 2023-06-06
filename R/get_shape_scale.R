0451343e5317676c32dbd26b6bf0079953a41f85
Théo Michelot <tm75@st-andrews.ac.uk>
2023-06-06 15:05:34 -0300

    change betas/alphas to ssf_par/tpm_par

--- R/get_shape_scale.R
+++ R/get_shape_scale.R
@@ -1,4 +1,4 @@
-##' Get the scale and scale from estimated betas
+##' Get the scale and scale from estimated ssf_par
 ##'
 ##' @param fit fitted model output
 ##' @param n_states number of fitted states
@@ -7,9 +7,9 @@
 
 get_shape_scale <- function(fit, n_states) {
 
-  # calculate gamma par from betas
-  scale <- - 1 / fit$betas$estimate[which(fit$betas$cov == "step")]
-  shape <- fit$betas$estimate[which(fit$betas$cov == "log(step)")] + 2
+  # calculate gamma par from ssf_par
+  scale <- - 1 / fit$ssf_par$estimate[which(fit$ssf_par$cov == "step")]
+  shape <- fit$ssf_par$estimate[which(fit$ssf_par$cov == "log(step)")] + 2
 
   par_df <- data.frame(state = rep(1:n_states),
                         shape = shape,
